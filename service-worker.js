00f6e966ae0e72502190606194ca15d84dfc681c
jjsxx <jjsxx4327@gmail.com>
2025-08-04 17:14:08 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('my-cache').then((cache) => {
+    caches.open('fpapp-cache-v1').then((cache) => {
       return cache.addAll([
         '/',
         '/index.html',
